dd0c952f412ed3753f9ba7cc85a39af689824e42
LeilaFerreira <leilaferreira.bhte@gmail.com>
2025-03-30 15:38:56 -0300

    inclui chatbot

---
+++ src/app/pagina-perguntas/pagina-perguntas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-perguntas',
+  imports: [],
+  templateUrl: './pagina-perguntas.component.html',
+  styleUrl: './pagina-perguntas.component.scss'
+})
+export class PaginaPerguntasComponent {
+
+}
